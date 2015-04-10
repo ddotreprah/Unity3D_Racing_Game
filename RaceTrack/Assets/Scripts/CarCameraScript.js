49fc29048b90072a6f8ae6b02f263d3340557926
JoshHarris85 <joshua.harris10@okstate.edu>
2015-04-09 20:27:27 -0500

    Initial Push of Game

---
+++ RaceTrack/Assets/Scripts/CarCameraScript.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+var car : Transform;
+var distance : float = 6.4;
+var height : float = 1.4;
+var rotationDamping : float = 3.0;
+var heightDamping : float = 2.0;
+var zoomRatio : float = 0.5;
+var defaultFOV : float = 60;
+private var rotationVector : Vector3;
+
+function Start () {
+
+}
+
+function LateUpdate () {
+    //keeps camera behind car at a traditional car racing angle
+    var wantedAngle = car.eulerAngles.y;
+    var wantedHeight = car.position.y + height;
+    var myAngle = transform.eulerAngles.y;
+    var myHeight = transform.position.y;
+    myAngle = Mathf.LerpAngle(myAngle,wantedAngle,rotationDamping * Time.deltaTime);
+    myHeight = Mathf.Lerp(myHeight,wantedHeight,heightDamping * Time.deltaTime);
+    var currentRotation = Quaternion.Euler(0,myAngle,0);
+    transform.position = car.position;
+    transform.position -= currentRotation*Vector3.forward*distance;
+    transform.position.y = myHeight;
+    transform.LookAt(car);
+}
+
+function FixedUpdate () {
+    //keeps camera behind car as it starts to go faster
+    var acc = car.rigidbody.velocity.magnitude;
+    camera.fieldOfView = defaultFOV + acc*zoomRatio*Time.deltaTime;
+
+}
---
+++ RaceTrack/Assets/Scripts/CarControlScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var wheelFL : WheelCollider;
+var wheelFR : WheelCollider;
+var wheelRL : WheelCollider;
+var wheelRR : WheelCollider;
+var maxTorque : float = 50;
+
+
+function Start () {
+    rigidbody.centerOfMass.y = -0.9;
+
+}
+
+function FixedUpdate () {
+    wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+    wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+    wheelFL.steerAngle = 10 * Input.GetAxis("Horizontal");
+    wheelFR.steerAngle = 10 * Input.GetAxis("Horizontal");
+}
