b63a95c4cf4b42baee750d95d1a96fdf325cc1da
blade93 <blademan93@gmail.com>
2015-04-21 12:56:07 -0500

    prefabs
    character prefabs

---
+++ RaceTrack/Assets/Scripts/VolumeMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeMenu : MonoBehaviour {
+
+	Font pauseMenuFont;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		
+		GUI.skin.box.font = pauseMenuFont;
+		GUI.skin.button.font = pauseMenuFont;
+
+			GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 250, 200), "Game Over");
+			
+			if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2, 250, 50), "Restart"))
+			{
+				Application.LoadLevel("Pool");
+			}
+			if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 50, 250, 50), "Quit Game"))
+			{
+				Application.Quit();
+			}
+		}
+
+}
