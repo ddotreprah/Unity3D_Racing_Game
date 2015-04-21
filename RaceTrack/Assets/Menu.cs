989f5aec25149927f9af84c58da6607d9dd464c3
ddotreprah <ddotreprah@gmail.com>
2015-04-21 15:03:45 -0500

    Debug
    Menu.cs had a compile error that I fixed.  There was an extra } at the
    end of the file.

--- RaceTrack/Assets/Menu.cs
+++ RaceTrack/Assets/Menu.cs
@@ -1,32 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour 
+{
 	private Animator _animator;
 	private CanvasGroup _canvasGroup;
 
-	public bool IsOpen{
+	public bool IsOpen
+    {
 		get { return _animator.GetBool ("IsOpen");}
 		set { _animator.SetBool ("IsOpen", value);}
 	}
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
-	public void Awake(){
+
+	public void Awake()
+    {
 		_animator = GetComponent<Animator>();
 
 		_canvasGroup = GetComponent<CanvasGroup>();
 
 		var rect = GetComponent<RectTransform>();
-		rect.offsetMax = rect.offsetMin = new Vector2 (0, 0);
-
+		rect.offsetMax = rect.offsetMin = new Vector2(0,0);
 	}
 
-	}
 }
+
