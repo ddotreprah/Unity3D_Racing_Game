36a77678e87402ac5768a49a0f841e6848995c28
ninogasper <gasper.anjano@okstate.edu>
2015-04-21 14:08:44 -0500

    3d

--- RaceTrack/Assets/Menu.cs
+++ RaceTrack/Assets/Menu.cs
@@ -2,7 +2,13 @@
 using System.Collections;
 
 public class Menu : MonoBehaviour {
+	private Animator _animator;
+	private CanvasGroup _canvasGroup;
 
+	public bool IsOpen{
+		get { return _animator.GetBool ("IsOpen");}
+		set { _animator.SetBool ("IsOpen", value);}
+	}
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +17,16 @@ public class Menu : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	public void Awake(){
+		_animator = GetComponent<Animator>();
+
+		_canvasGroup = GetComponent<CanvasGroup>();
+
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2 (0, 0);
+
+	}
+
 	}
 }
