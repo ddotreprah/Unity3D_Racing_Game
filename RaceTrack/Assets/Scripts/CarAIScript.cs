ae1b33991250b058caa17d06aab9ee2a49b47657
JoshHarris85 <joshua.harris10@okstate.edu>
2015-04-23 14:44:18 -0500

    Track1 basic AI

---
+++ RaceTrack/Assets/Scripts/CarAIScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarAIScript : MonoBehaviour {
+    private Transform[] waypoints;
+    private int currentWaypoint = 0;
+
+    public WheelCollider frontLeftWheel;
+    public WheelCollider frontRightWheel;
+    public GameObject waypointContainer;
+    public float speed = 10.0f;
+    private float rotationSpeed = 4.0f;
+    private float inputSteer = 0.0f;
+    private float steeringSharpness = 12.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the waypoint transforms.
+        Transform[] potentialWaypoints = waypointContainer.GetComponentsInChildren<Transform>();
+
+        waypoints = new Transform[potentialWaypoints.Length - 1];
+
+        print("PlayerScript:  " + potentialWaypoints.Length);
+
+        for (int i = 0, j = 0; i < potentialWaypoints.Length; i++)
+        {
+            if (potentialWaypoints[i] != waypointContainer.transform)
+            {
+                // This is not the container; add the waypoint to the array.
+                waypoints[j++] = potentialWaypoints[i];
+            }
+        }
+
+        GetComponent<Rigidbody>().velocity = Vector3.right * speed;
+    }
+
+    void FixedUpdate()
+    {
+        //float SpeedLimit = 100;
+
+        Vector3 movement = NavigateTowardWaypoint();
+
+        GetComponent<Rigidbody>().velocity = movement.normalized * speed * 100 * Time.deltaTime;
+
+        	frontLeftWheel.steerAngle = (steeringSharpness) * inputSteer;
+	        frontRightWheel.steerAngle = (steeringSharpness) * inputSteer;
+    }
+
+    Vector3 NavigateTowardWaypoint()
+    {
+        Vector3 relativeWaypointPosition =
+            waypoints[currentWaypoint].position - transform.position;
+
+        inputSteer = relativeWaypointPosition.x / relativeWaypointPosition.magnitude;
+
+        if (relativeWaypointPosition.magnitude < 1)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+            print("Current waypoint: " + currentWaypoint);
+            inputSteer = relativeWaypointPosition.x / relativeWaypointPosition.magnitude;
+        }
+
+        return relativeWaypointPosition;
+
+    }
+}
---
+++ RaceTrack/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Code from Dr.Mayfield's waypoint tutorial
+public class Waypoints : MonoBehaviour {
+
+    private static GameObject start;
+
+    public GameObject next;
+    public bool isStart = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        if(!next)
+        {
+            print("This waypoint is not connect " + this);
+        }
+
+        if(isStart)
+        {
+            start = gameObject;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
+        Gizmos.DrawCube(transform.position, new Vector3(1, 1, 1));
+
+        if (next)
+        {
+            Gizmos.color = new Color(0, 1, 0, 0.3f);
+            Gizmos.DrawLine(transform.position, next.transform.position);
+        }
+    }
+
+    Vector3 CalculateTargetPosition (Vector3 position)
+    {
+        if (Vector3.Distance(transform.position, position) < 1.2)
+        {
+            // We are getting close to the target waypoint;
+            // focus attention on next waypoint.
+            return next.transform.position;
+        }
+        else
+        {
+            // We still are far away from target waypoint.
+            return transform.position;
+        }
+    }
+}
