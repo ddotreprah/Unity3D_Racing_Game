600b142f1715bfd0cdc4256bf4a3adb87be7389b
blade93 <blademan93@gmail.com>
2015-04-26 00:37:21 -0500

    placement
    placement system set up for track 3. Few modification need to be made
    and system needs to be set up for other tracks.

---
+++ RaceTrack/Assets/Scripts/CarCheckpoint.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCheckpoint : MonoBehaviour {
+
+	public bool FirstHalf = false;
+	public bool SecondHalf = true;
+	public int lapCounter = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.gameObject.tag == "FinishLine") {
+			if (SecondHalf == true)
+			{
+				lapCounter++;
+				Debug.Log(lapCounter);
+				FirstHalf = true;
+				SecondHalf =false;
+			}
+		
+		}
+		if (collision.gameObject.tag == "HalfWay")
+		{
+			if (FirstHalf == true)
+			{
+				SecondHalf = true;
+				FirstHalf = false;
+			}
+		}
+
+	}
+
+}
--- RaceTrack/Assets/Scripts/CarControlScript.js
+++ RaceTrack/Assets/Scripts/CarControlScript.js
@@ -6,8 +6,21 @@ var wheelRR : WheelCollider;
 var maxTorque : float = 30;
 var isActive  = true;
 var maxSpeed : float = 20;
-
+var SecondHalf = true;
+var FirstHalf = false;
+var lapCounter : int = 0;
+var place : int;
+public var Rac1 : GameObject;
+public var Rac2 : GameObject;
+public var Racer1 : CarControlScript;
+public var Racer2 : CarControlScript;
 //Script from Easy Roads 3d example Unity store
+function Awake()
+{
+	Racer1 = Rac1.GetComponent(CarControlScript);
+	Racer2 = Rac2.GetComponent(CarControlScript);
+}
+
 function Start () {
 
     rigidbody.centerOfMass.y = -2.0f;
@@ -31,4 +44,46 @@ if(rigidbody.velocity.magnitude > maxSpeed)
 	}
 	
    
+}
+
+function OnTriggerEnter(collision : Collider)
+	{
+		
+		if (collision.gameObject.tag == "FinishLine") {
+			if (SecondHalf == true)
+			{
+				lapCounter++;
+				Debug.Log(lapCounter);
+				FirstHalf = true;
+				if (lapCounter == 1)
+					{
+						if ((Racer1.lapCounter < 1 && Racer2.lapCounter < 1))
+							{
+							
+								place = 1;
+							}
+						else if ((Racer1.lapCounter == 1 && Racer2.lapCounter < 1) || (Racer1.lapCounter < 1 && Racer2.lapCounter == 1))
+							{
+								place = 2;
+							}
+						else 
+							{
+								place = 3;
+							}
+						Debug.Log("Place");
+						Debug.Log(place);	
+					
+					}
+				SecondHalf =false;
+			}
+		
+		}
+		if (collision.gameObject.tag == "HalfWay")
+		{
+			if (FirstHalf == true)
+			{
+				SecondHalf = true;
+				FirstHalf = false;
+			}
+		}
 }
