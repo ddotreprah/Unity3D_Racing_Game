a1d670a089abc6c0cd20c6e73fbe1d44a8e175ba
ddotreprah <ddotreprah@gmail.com>
2015-04-27 17:54:44 -0500

    Sounds
    Added acceleration and collision sounds.  Need to refine the collision
    sound a bit.  When there is a wreck, the sound goes off too many times.
    
    Going to add an idle sound next.  For now, the only car noise is the
    acceleration when "W" is pressed.  Also need to add code for the xbox
    controller left joystick forward.

---
+++ RaceTrack/Assets/Scripts/CarSoundScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSoundScript : MonoBehaviour {
+
+    public AudioClip accel;
+    public AudioClip impact;
+    private AudioSource audio;
+
+	// Use this for initialization
+	void Start () 
+    {
+        //set the gameObjects AudioSource component to "audio"
+        audio = gameObject.GetComponent<AudioSource>();
+	}
+
+    //function is called when gameObject collides with another object
+    void OnCollisionEnter ()
+    {
+        //plays the collision impact sound
+        audio.PlayOneShot(impact);
+    }
+
+	// Update is called once per frame
+	void Update () 
+    {
+        //assigns the accelerate sound to a key or button.  Set to "W" for testing
+        //Need to assign this to Joystick forward on Xbox controller
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            audio.PlayOneShot(accel);
+        }
+    }
+}
