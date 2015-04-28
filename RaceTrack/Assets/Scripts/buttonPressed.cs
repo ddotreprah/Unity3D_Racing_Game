147e16c47e4b66f07dfe6adef47494b1bf193d6b
JoshHarris85 <joshua.harris10@okstate.edu>
2015-04-27 22:11:57 -0500

    Start Menu - start button goes to menu and exit game exits

---
+++ RaceTrack/Assets/Scripts/buttonPressed.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonPressed : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void startClicked()
+    {
+        Application.LoadLevel("LoadLevel");
+    }
+
+    public void learnMoreClicked()
+    {
+
+    }
+
+    public void aboutClicked()
+    {
+
+    }
+
+    public void optionsClicked()
+    {
+
+    }
+
+    public void exitClicked()
+    {
+        Application.Quit();
+
+    }
+}
